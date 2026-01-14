d43a27a5236de8b6c44a1f4f1561e1fe5761de5e
Umer Qurreshi <u.qurreshi34@gmail.com>
2026-01-14 17:39:25 +0000

    Updated ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-insecure-placeholder")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
