49b8db9daa1d268c3777bcbae70f2a04239accab
Umer Qurreshi <u.qurreshi34@gmail.com>
2026-01-15 00:04:21 +0000

    Accounted for null value

--- crypto/models.py
+++ crypto/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
 
 
@@ -15,7 +15,7 @@ class CryptoPrice(models.Model):
 
 
 class DataRefreshStatus(models.Model):
-    last_updated = models.DateTimeField()
+    last_updated = models.DateTimeField(default=now)
 
     def __str__(self):
         return f"Last refresh: {self.last_updated}"
